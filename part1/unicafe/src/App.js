cac1b72bd87263d05472dede3d6ad05b5a59e87e
Sumon Chattopadhyay <sumon.chattopadhyay@utah.edu>
2022-05-13 22:02:43 -0700

    Create table and remove errors for exercise 1.11

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -4,7 +4,6 @@ const StatisticLine = (props) => {
   return (
     <tr>
       <td>{props.text}</td>
-      &nbsp;
       <td>{props.value}</td>
     </tr>
   )
@@ -21,11 +20,15 @@ const Statistics = (props) => {
 
   return (
     <div>
-      <StatisticLine text="good" value={props.good} />
-      <StatisticLine text="neutral" value={props.neutral} />
-      <StatisticLine text="bad" value={props.bad} />
-      <StatisticLine text="average" value={(props.good - props.bad) / (props.good + props.neutral + props.bad)} />
-      <StatisticLine text="positive" value={props.good / (props.good + props.neutral + props.bad) * 100} />
+      <table>
+          <tbody>
+          <StatisticLine text="good" value={props.good} />
+          <StatisticLine text="neutral" value={props.neutral} />
+          <StatisticLine text="bad" value={props.bad} />
+          <StatisticLine text="average" value={(props.good - props.bad) / (props.good + props.neutral + props.bad)} />
+          <StatisticLine text="positive" value={props.good / (props.good + props.neutral + props.bad) * 100} />
+        </tbody>
+      </table>
     </div>
   )
 }
