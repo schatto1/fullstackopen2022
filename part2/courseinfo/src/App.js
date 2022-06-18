5928d69da9c1a39282728e9b75ae121882c5aaab
Sumon Chattopadhyay <sumon.chattopadhyay@utah.edu>
2022-06-17 22:05:36 -0700

    Add refactored code for exercise 2.1

---
+++ part2/courseinfo/src/App.js
@@ -0,0 +1,68 @@
+const Header = ({ course }) => <h1>{course}</h1>
+
+const Part = ({ part }) => {
+  return (
+    <div>
+      <p>{part.name} {part.exercises}</p>
+    </div>
+  )
+}
+
+const Content = ({ parts }) => {
+  return (
+    <div>
+      {parts.map(part => 
+        <Part key={part.id} part={part} />)}
+    </div>
+  )
+}
+
+const Course = ({ course }) => {
+  return (
+    <div>
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+    </div>
+  )
+}
+
+// const App = ({ notes }) => {
+//   return (
+//     <div>
+//       <h1>Notes</h1>
+//       <ul>
+//         {notes.map(note =>
+//           <Note key={note.id} note={note} />
+//         )}
+//       </ul>
+//     </div>
+//   )
+// }
+
+const App = () => {
+  const course = {
+    id: 1,
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      }
+    ]
+  }
+
+  return <Course course={course} />
+}
+
+export default App
