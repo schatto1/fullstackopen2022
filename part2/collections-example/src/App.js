8ff5e3467800a81aed6f9566c1513fa70aba7f41
Sumon Chattopadhyay <sumon.chattopadhyay@utah.edu>
2022-06-22 23:26:03 -0700

    Adds filtering functionality to example

--- part2/collections-example/src/App.js
+++ part2/collections-example/src/App.js
@@ -6,6 +6,7 @@ const App = (props) => {
   const [newNote, setNewNote] = useState(
     'a new note...'
   )
+  const [showAll, setShowAll] = useState(true)
 
   const addNote = (event) => {
     event.preventDefault()
@@ -25,11 +26,23 @@ const App = (props) => {
     setNewNote(event.target.value)
   }
 
+  const notesToShow = showAll
+    ? notes
+    : notes.filter(note => note.important === true)
+  // const result = condition ? val1 : val2
+  // can also be written as 
+  // notes.filter(note => note.important)
+
   return (
     <div>
       <h1>Notes</h1>
+      <div>
+        <button onClick={() => setShowAll(!showAll)}>
+          show {showAll ? 'important' : 'all' }
+        </button>
+      </div>
       <ul>
-        {notes.map(note => 
+        {notesToShow.map(note => 
           <Note key={note.id} note={note} />
         )}
       </ul>
