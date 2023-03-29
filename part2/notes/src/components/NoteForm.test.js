53d83bc9d32b35c7e56fcbc0e3ccac32bee17cc4
Sumon Chattopadhyay <sumon.chattopadhyay@utah.edu>
2023-03-29 11:03:35 -0700

    test: modifies test to get element by placeholder text

--- part2/notes/src/components/NoteForm.test.js
+++ part2/notes/src/components/NoteForm.test.js
@@ -4,17 +4,16 @@ import '@testing-library/jest-dom/extend-expect'
 import NoteForm from './NoteForm'
 import userEvent from '@testing-library/user-event'
 
-test('<NoteForm /> updates parent state and calls onSubmit', async () => {
+test('<NoteForm /> updates parent state and calls onSubmit', () => {
   const createNote = jest.fn()
-  const user = userEvent.setup()
 
   render(<NoteForm createNote={createNote} />)
 
-  const input = screen.getByRole('textbox')
+  const input = screen.getByPlaceholderText('write here note content')
   const sendButton = screen.getByText('save')
 
-  await user.type(input, 'testing a form...')
-  await user.click(sendButton)
+  userEvent.type(input, 'testing a form...')
+  userEvent.click(sendButton)
 
   expect(createNote.mock.calls).toHaveLength(1)
   expect(createNote.mock.calls[0][0].content).toBe('testing a form...')
