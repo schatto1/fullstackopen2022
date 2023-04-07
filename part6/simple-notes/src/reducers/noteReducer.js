7325f03d19273948f9b13543cd8d7c856611459f
Sumon Chattopadhyay <sumon.chattopadhyay@utah.edu>
2023-04-06 18:00:08 -0700

    feat: expands noteReducer to also toggle importance of note

--- part6/simple-notes/src/reducers/noteReducer.js
+++ part6/simple-notes/src/reducers/noteReducer.js
@@ -1,9 +1,21 @@
 const noteReducer = (state = [], action) => {
-  if (action.type === 'NEW_NOTE') {
-    return state.concat(action.payload)
+  switch(action.type) {
+    case 'NEW_NOTE':
+      return state.concat(action.payload)
+    case 'TOGGLE_IMPORTANCE': {
+      const id = action.payload.id
+      const noteToChange = state.find(n => n.id === id)
+      const changedNote = { 
+        ...noteToChange, 
+        important: !noteToChange.important 
+      }
+      return state.map(note =>
+        note.id !== id ? note : changedNote 
+      )
+     }
+    default:
+      return state
   }
-
-  return state
 }
 
 // eslint-disable-next-line import/no-anonymous-default-export
