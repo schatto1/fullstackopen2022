d23fe4dd8b4cf4ef4c408c1fe7186de1986e3393
Sumon Chattopadhyay <sumon.chattopadhyay@utah.edu>
2023-04-06 17:59:05 -0700

    test: adds test for adding new note

--- part6/simple-notes/src/reducers/noteReducer.js
+++ part6/simple-notes/src/reducers/noteReducer.js
@@ -1,11 +1,10 @@
 const noteReducer = (state = [], action) => {
   if (action.type === 'NEW_NOTE') {
-    state.push(action.payload)
-    return state
+    return state.concat(action.payload)
   }
 
   return state
 }
 
 // eslint-disable-next-line import/no-anonymous-default-export
-export default { noteReducer }
+export default noteReducer
