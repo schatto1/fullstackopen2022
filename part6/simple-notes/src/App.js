ade76ebabaeb0f66497e87ea7d6c5e6baadcf3c6
Sumon Chattopadhyay <sumon.chattopadhyay@utah.edu>
2023-04-11 15:41:44 -0700

    adds UI for chossing importance

--- part6/simple-notes/src/App.js
+++ part6/simple-notes/src/App.js
@@ -2,9 +2,21 @@ import Notes from "./components/Notes"
 import NewNote from "./components/NewNote"
 
 const App = () => {
-  return(
+  const filterSelected = (value) => {
+    console.log(value)
+  }
+
+  return (
     <div>
       <NewNote />
+      <div>
+        all          <input type="radio" name="filter"
+          onChange={() => filterSelected('ALL')} />
+        important    <input type="radio" name="filter"
+          onChange={() => filterSelected('IMPORTANT')} />
+        nonimportant <input type="radio" name="filter"
+          onChange={() => filterSelected('NONIMPORTANT')} />
+      </div>
       <Notes />
     </div>
   )
