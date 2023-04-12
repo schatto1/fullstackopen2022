1451a824dfd3c607773882f3818b6d3881430666
Sumon Chattopadhyay <sumon.chattopadhyay@utah.edu>
2023-04-12 11:16:09 -0700

    exercise 6.10 - chore: install redux toolkit

--- part6/simple-notes/src/index.js
+++ part6/simple-notes/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { configureStore } from 'redux'
+import { configureStore } from '@reduxjs/toolkit'
 import { Provider } from 'react-redux'
 
 import App from './App'
