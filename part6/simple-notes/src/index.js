ac70de130b1bf5f031d8bf0296ba9debf0107e9e
Sumon Chattopadhyay <sumon.chattopadhyay@utah.edu>
2023-04-11 18:29:42 -0700

    refactor: index.js now uses redux toolkit configureStore

--- part6/simple-notes/src/index.js
+++ part6/simple-notes/src/index.js
@@ -1,18 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { createStore, combineReducers } from 'redux'
+import { configureStore } from 'redux'
 import { Provider } from 'react-redux'
 
 import App from './App'
 import noteReducer from './reducers/noteReducer'
 import filterReducer from './reducers/filterReducer'
 
-const reducer = combineReducers({
-  notes: noteReducer,
-  filter: filterReducer
+const store = configureStore({
+  reducer: {
+    notes: noteReducer,
+    filter: filterReducer
+  }
 })
 
-const store = createStore(reducer)
+console.log(store.getState())
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
