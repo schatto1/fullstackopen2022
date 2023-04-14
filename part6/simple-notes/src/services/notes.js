1b5e7c7effa070322ea96596f795740a863f29af
Sumon Chattopadhyay <sumon.chattopadhyay@utah.edu>
2023-04-13 22:03:02 -0700

    createNote now uses backend as well

--- part6/simple-notes/src/services/notes.js
+++ part6/simple-notes/src/services/notes.js
@@ -7,5 +7,13 @@ const getAll = async () => {
   return response.data
 }
 
-// eslint-disable-next-line import/no-anonymous-default-export
-export default { getAll }
+const createNew = async (content) => {
+  const object = { content, important: false }
+  const response = await axios.post(baseUrl, object)
+  return response.data
+}
+
+export default {
+  getAll,
+  createNew,
+}
