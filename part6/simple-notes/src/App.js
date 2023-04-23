b2fc7d03ba3c9e23a1c56365b8e04b4f823477cf
Sumon Chattopadhyay <sumon.chattopadhyay@utah.edu>
2023-04-22 20:53:15 -0700

    fix: App was completely wrong. now fixed

--- part6/simple-notes/src/App.js
+++ part6/simple-notes/src/App.js
@@ -1,13 +1,25 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { Provider } from 'react-redux' 
-import store from './store'
-import App from './App'
+import { useEffect } from 'react'
+import Notes from "./components/Notes"
+import NewNote from "./components/NewNote"
+import VisibilityFilter from './components/VisibilityFilter'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <Provider store={store}>
-    <App />
-  </Provider>
-)
+import { initializeNotes } from './reducers/noteReducer'
+import { useDispatch } from 'react-redux'
+
+const App = () => {
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    dispatch(initializeNotes()) 
+  }, [dispatch]) 
+
+  return(
+    <div>
+      <NewNote />
+      <VisibilityFilter />
+      <Notes />
+    </div>
+  )
+}
 
 export default App
