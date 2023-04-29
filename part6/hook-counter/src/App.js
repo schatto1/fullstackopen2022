09b954eacea8aed192befc3a172aeb95d1d14283
Sumon Chattopadhyay <sumon.chattopadhyay@utah.edu>
2023-04-29 15:43:45 -0700

    chore: synchs hook-counter with end of part6-3
    Moves Context out of App
    Defines Context helper functions to make code more elegant

--- part6/hook-counter/src/App.js
+++ part6/hook-counter/src/App.js
@@ -1,31 +1,16 @@
-import { useReducer } from 'react'
-import CounterContext from './CounterContext'
-
-const counterReducer = (state, action) => {
-  switch (action.type) {
-    case "INC":
-        return state + 1
-    case "DEC":
-        return state - 1
-    case "ZERO":
-        return 0
-    default:
-        return state
-  }
-}
+import Display from './components/Display'
+import Button from './components/Button'
 
 const App = () => {
-  const [counter, counterDispatch] = useReducer(counterReducer, 0)
-
   return (
-    <CounterContext.Provider value={[counter, counterDispatch]}>
-      <div>{counter}</div>
+    <div>
+      <Display />
       <div>
-        <button onClick={() => counterDispatch({ type: "INC" })}>+</button>
-        <button onClick={() => counterDispatch({ type: "DEC" })}>-</button>
-        <button onClick={() => counterDispatch({ type: "ZERO" })}>0</button>
+        <Button type='INC' label='+' />
+        <Button type='DEC' label='-' />
+        <Button type='ZERO' label='0' />
       </div>
-    </CounterContext.Provider>
+    </div>
   )
 }
 
