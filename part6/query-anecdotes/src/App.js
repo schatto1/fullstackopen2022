5b61ed40d9c69acab9ffbd175ae7d20957d0d8a9
Sumon Chattopadhyay <sumon.chattopadhyay@utah.edu>
2023-05-01 18:08:30 -0700

    exercise 6.23 - feat: notification system implemented using useState

--- part6/query-anecdotes/src/App.js
+++ part6/query-anecdotes/src/App.js
@@ -2,8 +2,27 @@ import AnecdoteForm from './components/AnecdoteForm'
 import Notification from './components/Notification'
 import { useQuery, useMutation, useQueryClient } from 'react-query'
 import { getAnecdotes, updateAnecdote } from './services/requests'
+import { useReducer } from 'react'
+import { useState } from 'react'
+
+const notificationReducer = (notification, action) => {
+  switch (action.type) {
+    case "ON":
+      console.log("ON was called")
+      console.log(notification)
+      return notification
+    case "OFF":
+      return null
+    default:
+      return notification
+  }
+}
 
 const App = () => {
+
+  const [notification, setNotification] = useState(null)
+
+  // const [notification, dispatch] = useReducer(notificationReducer, null)
 
   const queryClient = useQueryClient()
 
@@ -14,7 +33,20 @@ const App = () => {
   })
 
   const handleVote = (anecdote) => {
+    const message = 'You voted for "' + anecdote.content + '"'
     updateAnecdoteMutation.mutate({...anecdote, votes: anecdote.votes+1 })
+    setNotification(message)
+    setTimeout(() => {
+        setNotification(null)
+      }, 5000)
+
+    // dispatch({
+    //   type: 'ON',
+    //   notification: message
+    // })
+    // setTimeout(() => {
+    //   dispatch({type: 'OFF'})
+    // }, 5000)
   }
   
   const result = useQuery('anecdotes', getAnecdotes)
@@ -33,7 +65,7 @@ const App = () => {
     <div>
       <h3>Anecdote app</h3>
     
-      <Notification />
+      <Notification notification={notification}/>
       <AnecdoteForm />
     
       {anecdotes.map(anecdote =>
--- part6/query-anecdotes/src/NotificationContext.js
+++ part6/query-anecdotes/src/NotificationContext.js
@@ -1,4 +1,15 @@
-import { createContext, useReducer, useContex } from "react";
+import { createContext, useReducer, useContext } from "react";
+
+const notificationReducer = (state, action) => {
+  switch (action.type) {
+    case "ON":
+      return state
+    case "OFF":
+      return null
+    default:
+      return state
+  }
+}
 
 const NotificationContext = createContext()
 
