773c15116a11ba91982a4c9e9efb35ffd2eff38d
Sumon Chattopadhyay <sumon.chattopadhyay@utah.edu>
2023-05-26 15:53:36 -0700

    exercise 7.1 - adds React Router to application
    Menu component now controls view

--- part7/routed-anecdotes/src/App.js
+++ part7/routed-anecdotes/src/App.js
@@ -1,14 +1,24 @@
 import { useState } from 'react'
+import { Routes, Route, Link } from 'react-router-dom'
 
-const Menu = () => {
+const Menu = ({anecdotes, addNew}) => {
   const padding = {
     paddingRight: 5
   }
   return (
     <div>
-      <a href='#' style={padding}>anecdotes</a>
-      <a href='#' style={padding}>create new</a>
-      <a href='#' style={padding}>about</a>
+      <div>
+        <Link style={padding} to="/anecdotes">anecdotes</Link>
+        <Link style={padding} to="/create">create new</Link>
+        <Link style={padding} to="/about">about</Link>
+      </div>
+      <Routes>
+        <Route path="/" element={<AnecdoteList anecdotes={anecdotes}/>}/>
+        <Route path="/anecdotes" element={<AnecdoteList anecdotes={anecdotes}/>}/>
+        <Route path="/create" element={<CreateNew addNew={addNew} />}/>
+        <Route path="/about" element={<About />}/>
+      </Routes>
+      <div></div>
     </div>
   )
 }
@@ -125,10 +135,7 @@ const App = () => {
   return (
     <div>
       <h1>Software anecdotes</h1>
-      <Menu />
-      <AnecdoteList anecdotes={anecdotes} />
-      <About />
-      <CreateNew addNew={addNew} />
+      <Menu anecdotes={anecdotes} addNew={addNew}/>
       <Footer />
     </div>
   )
