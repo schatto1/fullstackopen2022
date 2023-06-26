c619d9f5f2dd31853435f32b6e4bff9fc4c7a4ab
Sumon Chattopadhyay <sumon.chattopadhyay@utah.edu>
2023-06-26 14:53:45 -0700

    exercise 7.6 - fix: deal with Invalid value for prop `reset` on <input> tag warning

--- part7/routed-anecdotes/src/App.js
+++ part7/routed-anecdotes/src/App.js
@@ -84,9 +84,9 @@ const CreateNew = (props) => {
 
   const handleReset = (e) => {
     e.preventDefault()
-    content.reset()
-    author.reset()
-    info.reset()
+    content.onReset()
+    author.onReset()
+    info.onReset()
   }
 
   return (
