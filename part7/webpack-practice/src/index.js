c3475099fda1ad0f785ce4eab8709faf65f7d097
Sumon Chattopadhyay <sumon.chattopadhyay@utah.edu>
2023-07-07 22:25:45 -0700

    transpiler section

--- part7/webpack-practice/src/index.js
+++ part7/webpack-practice/src/index.js
@@ -1,7 +1,7 @@
-import App from './App';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import 'core-js/stable/index.js'
+import 'regenerator-runtime/runtime.js'
 
-const hello = name => {
-  console.log(`hello ${name}`)
-}
-
-App()
+ReactDOM.createRoot(document.getElementById('root')).render(<App />)
--- part7/webpack-practice/webpack.config.js
+++ part7/webpack-practice/webpack.config.js
@@ -6,7 +6,20 @@ const config = () => {
     output: {
       path: path.resolve(__dirname, 'build'),
       filename: 'main.js'
-    }
+    },
+
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          loader: 'babel-loader',
+          options: {
+        
+            presets: ['@babel/preset-env', '@babel/preset-react']
+          }
+        },
+      ],
+    },
   }
 }
 
