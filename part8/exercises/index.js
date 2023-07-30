5519802dea3a06efc3d5a8fcb891c9b16816df52
Sumon Chattopadhyay <sumon.chattopadhyay@utah.edu>
2023-07-29 21:01:04 -0700

    exercise 8.2 - feat: implements allBooks query

--- part8/exercises/index.js
+++ part8/exercises/index.js
@@ -98,17 +98,29 @@ let books = [
 */
 
 const typeDefs = `
-  type Query {
-    bookCount: Int!
-    authorCount: Int!
-  }
+
+type Book {
+  title: String!
+  published: Int!
+  author: String!
+  id: ID!
+  genres: [String!]!
+}
+
+type Query {
+  bookCount: Int!
+  authorCount: Int!
+  allBooks: [Book!]
+
+}
+
 `
 
 const resolvers = {
   Query: {
     bookCount: () => books.length,
-    authorCount: () => authors.length
-
+    authorCount: () => authors.length,
+    allBooks: () => books,
   }
 }
 
