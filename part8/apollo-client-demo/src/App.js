ddf3d0d23fc1b1ebdb82df579906c0ea720d50c3
Sumon Chattopadhyay <sumon.chattopadhyay@utah.edu>
2023-08-16 16:24:06 -0700

    synchs example code with end of 8-9

--- part8/apollo-client-demo/src/App.js
+++ part8/apollo-client-demo/src/App.js
@@ -1,31 +1,73 @@
 import { useState } from 'react'
-import { useQuery, useApolloClient } from '@apollo/client'
+import { useQuery, useApolloClient, useSubscription } from '@apollo/client'
 
 import Persons from './components/Persons'
 import PersonForm from './components/PersonForm'
 import PhoneForm from './components/PhoneForm'
 import LoginForm from './components/LoginForm'
 
-import { ALL_PERSONS } from './queries'
+import { ALL_PERSONS, PERSON_ADDED } from './queries'
 
-const Notify = ({errorMessage}) => {
-  if ( !errorMessage ) {
+const Notify = ({ errorMessage }) => {
+  if (!errorMessage) {
     return null
   }
-  return (
-    <div style={{color: 'red'}}>
-      {errorMessage}
-    </div>
-  )
+  return <div style={{ color: 'red' }}>{errorMessage}</div>
+}
+
+export const updateCache = (cache, query, addedPerson) => {
+  const uniqByName = (a) => {
+    let seen = new Set()
+    return a.filter((item) => {
+      let k = item.name
+      return seen.has(k) ? false : seen.add(k)
+    })
+  }
+
+  cache.updateQuery(query, ({ allPersons }) => {
+    return {
+      allPersons: uniqByName(allPersons.concat(addedPerson)),
+    }
+  })
 }
 
 const App = () => {
+  const result = useQuery(ALL_PERSONS)
   const [errorMessage, setErrorMessage] = useState(null)
   const [token, setToken] = useState(null)
-  const result = useQuery(ALL_PERSONS)
   const client = useApolloClient()
 
-  if (result.loading)  {
+  useSubscription(PERSON_ADDED, {
+    onData: ({ data, client }) => {
+      const addedPerson = data.data.personAdded
+      notify(`${addedPerson.name} added`)
+      updateCache(client.cache, { query: ALL_PERSONS }, addedPerson)
+
+      /*
+      const uniqByName = (a) => {
+        let seen = new Set()
+        return a.filter((item) => {
+          let k = item.name
+          return seen.has(k) ? false : seen.add(k)
+        })
+      }
+
+      client.cache.updateQuery({ query: ALL_PERSONS }, ({ allPersons }) => {
+        return {
+          allPersons: uniqByName(allPersons.concat(addedPerson)),
+        }
+      })
+      */
+    },
+  })
+
+  const logout = () => {
+    setToken(null)
+    localStorage.clear()
+    client.resetStore()
+  }
+
+  if (result.loading) {
     return <div>loading...</div>
   }
 
@@ -38,31 +80,21 @@ const App = () => {
 
   if (!token) {
     return (
-      <div>
+      <>
         <Notify errorMessage={errorMessage} />
-        <h2>Login</h2>
-        <LoginForm
-          setToken={setToken}
-          setError={notify}
-        />
-      </div>
+        <LoginForm setToken={setToken} setError={notify} />
+      </>
     )
   }
 
-  const logout = () => {
-    setToken(null)
-    localStorage.clear()
-    client.resetStore()
-  }
-
   return (
-    <div>
+    <>
       <Notify errorMessage={errorMessage} />
       <button onClick={logout}>logout</button>
       <Persons persons={result.data.allPersons} />
       <PersonForm setError={notify} />
       <PhoneForm setError={notify} />
-    </div>
+    </>
   )
 }
 
