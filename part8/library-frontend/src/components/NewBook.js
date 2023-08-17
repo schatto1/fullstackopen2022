23393c9926ee3894942d11e4fd67292369770eee
Sumon Chattopadhyay <sumon.chattopadhyay@utah.edu>
2023-08-16 21:54:58 -0700

    exercise 8.26 - fix

--- part8/library-frontend/src/components/NewBook.js
+++ part8/library-frontend/src/components/NewBook.js
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import { useMutation } from '@apollo/client'
-import { ADD_BOOK, ALL_BOOKS, ALL_AUTHORS } from '../queries'
+import { ADD_BOOK, ALL_BOOKS } from '../queries'
 
 import { updateCache } from '../App'
 
