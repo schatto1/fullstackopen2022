9d06dc00ce12dbbf72eda9bcb0ec10ea4e503d14
Sumon Chattopadhyay <sumon.chattopadhyay@utah.edu>
2023-08-24 11:22:38 -0700

    exercise 9.11 - feat: sets up GET endpoint /api/patients
    Excludes ssn from return to frontend using utility type
    Adds cors to solve data fetch issue from frontend

--- part9/patientor/backend/src/index.ts
+++ part9/patientor/backend/src/index.ts
@@ -1,8 +1,15 @@
 import express from 'express';
+import cors from 'cors';
 
 import diagnosesRouter from './routes/diagnoses';
+import patientsRouter from './routes/patients';
 
 const app = express();
+
+
+// eslint-disable-next-line @typescript-eslint/no-unsafe-call
+app.use(cors());
+
 app.use(express.json());
 
 const PORT = 3001;
@@ -13,6 +20,7 @@ app.get('/api/ping', (_req, res) => {
 });
 
 app.use('/api/diagnoses', diagnosesRouter);
+app.use('/api/patients', patientsRouter);
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
