c7ba0702b0d11584a2e1868a3f71218ff5676f1b
Sumon Chattopadhyay <sumon.chattopadhyay@utah.edu>
2023-08-25 16:03:39 -0700

    exercise 9.13 - refactor: gender field now uses enum type
    Safe parsing, calidation, and type guard set up for api/patients POST request

--- part9/patientor/backend/src/routes/patients.ts
+++ part9/patientor/backend/src/routes/patients.ts
@@ -1,7 +1,8 @@
-/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import express from 'express';
 
 import patientsService from '../services/patientsService';
+
+import toNewPatient from '../utils';
 
 const router = express.Router();
 
@@ -21,15 +22,18 @@ router.get('/:id', (req, res) => {
 
 
 router.post('/', (req, res) => {
-  const { name, dateOfBirth, ssn, gender, occupation } = req.body;
-  const addedPatient = patientsService.addPatient({
-    name,
-    dateOfBirth,
-    ssn,
-    gender,
-    occupation,
-  });
-  res.json(addedPatient);
+  try {
+    const newPatient = toNewPatient(req.body);
+
+    const addedPatient = patientsService.addPatient(newPatient);
+    res.json(addedPatient);
+  } catch (error: unknown) {
+    let errorMessage = 'Something went wrong.';
+    if (error instanceof Error) {
+      errorMessage += ' Error: ' + error.message;
+    }
+    res.status(400).send(errorMessage);
+  }
 });
 
 export default router;
---
+++ part9/patientor/backend/src/utils.ts
@@ -0,0 +1,57 @@
+import { NewPatient, Gender } from './types';
+
+const toNewPatient = (object: unknown): NewPatient => {
+  if ( !object || typeof object !== 'object' ) {
+    throw new Error('Incorrect or missing data');
+  }
+
+  if ('name' in object && 'dateOfBirth' in object && 'ssn' in object && 'gender' in object && 'occupation' in object)  {
+    const newPatient: NewPatient = {
+      name: parseString(object.name),
+      dateOfBirth: parseDate(object.dateOfBirth),
+      ssn: parseString(object.ssn),
+      gender: parseGender(object.gender),
+      occupation: parseString(object.occupation)
+    };
+  
+    return newPatient;
+  }
+
+  throw new Error('Incorrect data: some fields are missing');
+};
+
+const isString = (text: unknown): text is string => {
+  return typeof text === 'string' || text instanceof String;
+};
+
+const parseString = (text: unknown): string => {
+  if (!text || !isString(text)) {
+    throw new Error('Incorrect or missing text');
+  }
+
+  return text;
+};
+
+const isDate = (date: string): boolean => {
+  return Boolean(Date.parse(date));
+};
+
+const parseDate = (date: unknown): string => {
+  if (!date || !isString(date) || !isDate(date)) {
+      throw new Error('Incorrect or missing date: ' + date);
+  }
+  return date;
+};
+
+const isGender = (param: string): param is Gender => {
+  return Object.values(Gender).map(v => v.toString()).includes(param);
+};
+
+const parseGender = (gender: unknown): Gender => {
+  if (!isString(gender) || !isGender(gender)) {
+      throw new Error('Incorrect gender: ' + gender);
+  }
+  return gender;
+};
+
+export default toNewPatient;
