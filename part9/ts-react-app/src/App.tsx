8bbbc1d3fca1fc5df53f880ae691ecd7f1b9eea3
Sumon Chattopadhyay <sumon.chattopadhyay@utah.edu>
2023-08-25 22:48:52 -0700

    exercise 9.15 - chore: copies the type information and variable courseParts from material

--- part9/ts-react-app/src/App.tsx
+++ part9/ts-react-app/src/App.tsx
@@ -1,29 +1,17 @@
 import Content from "./components/Content";
 import Header from "./components/Header";
 import Total from "./components/Total";
+import { courseParts } from "./types";
 
 const App = () => {
   const courseName = "Half Stack application development";
-  const courseParts = [
-    {
-      name: "Fundamentals",
-      exerciseCount: 10
-    },
-    {
-      name: "Using props to pass data",
-      exerciseCount: 7
-    },
-    {
-      name: "Deeper type usage",
-      exerciseCount: 14
-    }
-  ];
+  const parts = courseParts;
 
   return (
     <div>
       <Header name={courseName} />
-      <Content parts={courseParts} />
-      <Total parts={courseParts} />
+      <Content parts={parts} />
+      <Total parts={parts} />
     </div>
   );
 };
--- part9/ts-react-app/src/types.ts
+++ part9/ts-react-app/src/types.ts
@@ -2,15 +2,67 @@ export interface HeaderProps {
   name: string
 }
 
-export interface Part {
-  name: string,
-  exerciseCount: number
-}
-
 export interface PartProps {
-  part: Part
+  part: CoursePart
 }
 
 export interface CourseProps {
-  parts: Array<Part>
+  parts: Array<CoursePart>
 }
+
+interface CoursePartBase {
+  name: string;
+  exerciseCount: number;
+}
+
+interface CoursePartBasic extends CoursePartBase {
+  description: string;
+  kind: "basic"
+}
+
+interface CoursePartGroup extends CoursePartBase {
+  groupProjectCount: number;
+  kind: "group"
+}
+
+interface CoursePartBackground extends CoursePartBase {
+  description: string;
+  backgroundMaterial: string;
+  kind: "background"
+}
+
+type CoursePart = CoursePartBasic | CoursePartGroup | CoursePartBackground;
+
+export const courseParts: CoursePart[] = [
+  {
+    name: "Fundamentals",
+    exerciseCount: 10,
+    description: "This is an awesome course part",
+    kind: "basic"
+  },
+  {
+    name: "Using props to pass data",
+    exerciseCount: 7,
+    groupProjectCount: 3,
+    kind: "group"
+  },
+  {
+    name: "Basics of type Narrowing",
+    exerciseCount: 7,
+    description: "How to go from unknown to string",
+    kind: "basic"
+  },
+  {
+    name: "Deeper type usage",
+    exerciseCount: 14,
+    description: "Confusing description",
+    backgroundMaterial: "https://type-level-typescript.com/template-literal-types",
+    kind: "background"
+  },
+  {
+    name: "TypeScript in frontend",
+    exerciseCount: 10,
+    description: "a hard part",
+    kind: "basic",
+  },
+];
