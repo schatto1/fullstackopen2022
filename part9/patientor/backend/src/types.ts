63058d8895d6d6e3e1b90a7d38ccec29f5fda5a3
Sumon Chattopadhyay <sumon.chattopadhyay@utah.edu>
2023-08-30 16:55:35 -0700

    preps patientor backend types for exercise 9.22

--- part9/patientor/backend/src/types.ts
+++ part9/patientor/backend/src/types.ts
@@ -10,9 +10,46 @@ export interface Diagnosis {
   latin?: string;
 }
 
-// eslint-disable-next-line @typescript-eslint/no-empty-interface
-export interface Entry {
+interface BaseEntry {
+  id: string;
+  description: string;
+  date: string;
+  specialist: string;
+
+  diagnosisCodes?: Array<Diagnosis['code']>;
 }
+
+export enum HealthCheckRating {
+  "Healthy" = 0,
+  "LowRisk" = 1,
+  "HighRisk" = 2,
+  "CriticalRisk" = 3
+}
+
+interface HealthCheckEntry extends BaseEntry {
+  type: "HealthCheck";
+  healthCheckRating: HealthCheckRating;
+}
+
+// eslint-disable-next-line @typescript-eslint/no-empty-interface
+interface OccupationalHealthcareEntry extends BaseEntry {
+
+}
+
+// eslint-disable-next-line @typescript-eslint/no-empty-interface
+interface HospitalEntry extends BaseEntry {
+
+}
+
+export type Entry =
+  | HospitalEntry
+  | OccupationalHealthcareEntry
+  | HealthCheckEntry;
+
+// Define special omit for unions
+type UnionOmit<T, K extends string | number | symbol> = T extends unknown ? Omit<T, K> : never;
+// Define Entry without the 'id' property
+export type EntryWithoutId = UnionOmit<Entry, 'id'>;
 
 export interface Patient {
   id: string;
