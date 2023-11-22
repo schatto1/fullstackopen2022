01f14589e6d359c324cdfe99a767c1b799a59627
Sumon Chattopadhyay <sumon.chattopadhyay@utah.edu>
2023-11-21 16:43:55 -0800

    exercise 13.4 - feat: adds GET, POST, and DELETE operations to web app

--- part13/blogs-app/backend/index.js
+++ part13/blogs-app/backend/index.js
@@ -15,46 +15,63 @@ const sequelize = new Sequelize(process.env.DATABASE_URL, {
   },
 });
 
-// class Note extends Model {}
-// Note.init({
-//   id: {
-//     type: DataTypes.INTEGER,
-//     primaryKey: true,
-//     autoIncrement: true
-//   },
-//   content: {
-//     type: DataTypes.TEXT,
-//     allowNull: false
-//   },
-//   important: {
-//     type: DataTypes.BOOLEAN
-//   },
-//   date: {
-//     type: DataTypes.DATE
-//   }
-// }, {
-//   sequelize,
-//   underscored: true,
-//   timestamps: false,
-//   modelName: 'note'
-// })
+class Blog extends Model {}
+Blog.init({
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  author: {
+    type: DataTypes.TEXT,
+  },
+  url: {
+    type: DataTypes.TEXT,
+    allowNull: false
+  },
+  title: {
+    type: DataTypes.TEXT,
+    allowNull: false
+  },
+  likes: {
+    type: DataTypes.INTEGER,
+    defaultValue: 0
+  }
+}, {
+  sequelize,
+  underscored: true,
+  timestamps: false,
+  modelName: 'blog'
+})
+
+Blog.sync()
 
 app.get('/api/blogs', async (req, res) => {
-  await sequelize.authenticate()
-  const blogs = await sequelize.query("SELECT * FROM blogs", { type: QueryTypes.SELECT })
-  // const notes = await Note.findAll()
-  sequelize.close()
+  const blogs = await Blog.findAll()
+
+  console.log(JSON.stringify(blogs))
+
   res.json(blogs)
 })
 
-// app.post('/api/notes', async (req, res) => {
-//   try {
-//     const note = await Note.create(req.body)
-//     return res.json(note)
-//   } catch(error) {
-//     return res.status(400).json({ error })
-//   }
-// })
+app.post('/api/blogs', async (req, res) => {
+  try {
+    const blog = await Blog.create(req.body)
+    return res.json(blog)
+  } catch(error) {
+    return res.status(400).json({ error })
+  }
+})
+
+app.delete('/api/blogs/:id', async (req, res) => {
+  const blog = await Blog.findByPk(req.params.id)
+  if (blog) {
+    await blog.destroy()
+    res.json(blog)
+  } else {
+    res.status(404).end()
+  }
+})
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
