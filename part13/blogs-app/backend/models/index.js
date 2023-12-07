4095325700c03436ee08a9662d3812c37c5082fd
Sumon Chattopadhyay <sumon.chattopadhyay@utah.edu>
2023-12-07 10:22:42 -0800

    exercise 13.19 - refactor: add support for reading list using connection table and migrations
    Adding to reading list and displaying list can only be done directly from
    database

--- part13/blogs-app/backend/models/index.js
+++ part13/blogs-app/backend/models/index.js
@@ -1,8 +1,12 @@
 const Blog = require('./blog')
 const User = require('./user')
+const ReadingList = require('./reading_list')
 
 User.hasMany(Blog)
 Blog.belongsTo(User)
+
+User.belongsToMany(Blog, { through: ReadingList, as: 'saved_blogs' })
+Blog.belongsToMany(User, { through: ReadingList, as: 'users_saved' })
 
 // comment out below since using migrations
 // Blog.sync({ alter: true })
